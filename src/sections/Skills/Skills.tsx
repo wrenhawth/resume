a305072bdd440f178fa23afdedb40e1bd9a5175d
Josh Robusto <joshuarobusto@gmail.com>
2019-06-24 21:15:36 -0700

    Slight expanding and reword of skills section

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -3,9 +3,6 @@ import { SectionName } from "../../components/SectionName";
 import { Icon } from "../../components/Icon";
 import { styled } from "../../utils/theme";
 
-interface SkillsProps {
-}
-
 const SkillsList = styled.div`
     font-family: ${props => props.theme.headerFont}
 `;
@@ -14,15 +11,15 @@ const Level = styled.span`
     font-weight: normal;
 `
 
-export function Skills(props: SkillsProps): JSX.Element {
+export function Skills(): JSX.Element {
 
     return (
         <div>
             <SectionName><Icon type="construct"/> Skills</SectionName>
             <SkillsList>
                 <p><Level>Experienced</Level>: React, TypeScript, Node.js, HTML, SCSS, MongoDB</p>
-                <p><Level>Working Knowledge</Level>: Python, SQL, Meteor, Docker, Express, Amazon SQS, Amazon Labmda</p>
-                <p><Level>Previous Experience</Level>: AngularJS, Express</p>
+                <p><Level>Working Knowledge</Level>: Python, SQL, Meteor, Mocha, Docker, Express, Amazon SQS, Amazon Labmda</p>
+                <p><Level>Previous Exposure</Level>: AngularJS, Storybook, Webpack, Express</p>
             </SkillsList>
         </div>
     );
